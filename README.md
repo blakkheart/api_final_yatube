95289cbd07fbd6e5dbe786db4c929b3cfa0245a7
Vlad Y <blakkheart66@gmail.com>
2023-08-05 18:55:24 +0300

    ready to be reviewed

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # api_final
-api final
+
+api_final is a Python DRF API for yatube project (front-end is not included).
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install api_final's dependencies.
+
+```bash
+(venv) pip install -r requirements.txt
+```
+
+## Usage
+
+```python
+
+# returns all posts
+'GET' request to /api/v1/posts/
+# output example JSON:
+{
+  "count": 123,
+  "next": "http://api.example.org/accounts/?offset=400&limit=100",
+  "previous": "http://api.example.org/accounts/?offset=200&limit=100",
+  "results": [
+    {
+      "id": 0,
+      "author": "string",
+      "text": "string",
+      "pub_date": "2021-10-14T20:41:29.648Z",
+      "image": "string",
+      "group": 0
+    }
+  ]
+}
+
+# you can see all allowed requests and examples in <static/redoc.yaml>
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[YaPracticum](https://practicum.yandex.ru)
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,16 +1,16 @@
-from rest_framework import serializers
+from rest_framework import serializers, validators
 from rest_framework.relations import SlugRelatedField
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group', )
+        read_only_fields = ('author', 'id', 'pub_date', 'image',)
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -19,5 +19,44 @@ class CommentSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = ('id', 'author', 'post', 'text', 'created',)
+        read_only_fields = ('author', 'id', 'created', 'post')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(slug_field='username',
+                                             queryset=User.objects.all())
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following',)
+        read_only_fields = ('user',)
+        validators = [
+            validators.UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message='Its not allowed to follow somebody twice!'
+            )
+        ]
+
+    def validate_following(self, value):
+        user = self.context.get('request').user
+        if value == user:
+            raise serializers.ValidationError(
+                'Its not allowed to follow yourself!'
+            )
+        return value
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description',)
+        read_only_fields = ('id', 'slug', 'description')
